033e30c7f49183e66e6b52ea31096d46ec6d4226
Kah Siong Tan <jax79sg@yahoo.com.sg>
2021-01-13 15:28:34 +0800

    Update 01-download-essentials.sh

--- v1.19/01-download-essentials.sh
+++ v1.19/01-download-essentials.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 ## Download essentials
 mkdir -p 01_essentials
