91f0f69bae82899d1e4f43b6187755a0487da211
Kah Siong Tan <jax79sg@yahoo.com.sg>
2021-02-22 14:17:08 +0800

    Update 01-download-essentials.sh

--- v1.19/01-download-essentials.sh
+++ v1.19/01-download-essentials.sh
@@ -12,8 +12,6 @@ sudo apt-get install --reinstall -y liberror-perl git-man git vim net-tools buil
 sudo mv /var/cache/apt/archives/*.deb 01_essentials/
 sudo apt-get install --reinstall -y liberror-perl git-man git vim net-tools build-essential openssh-server apt-transport-https curl
 sudo rm /var/cache/apt/archives/*.deb
-
-
 ## Download docker 19.03
 mkdir -p 02_docker
 cat > 02_docker/docker.service << EOF
@@ -31,8 +29,6 @@ ExecStart=/usr/bin/dockerd
 [Install]
 WantedBy=multi-user.target
 EOF
-
-
 wget https://download.docker.com/linux/static/stable/x86_64/docker-19.03.9.tgz
 mv docker-19.03.9.tgz 02_docker/
 tar -xvf 02_docker/docker-19.03.9.tgz 
@@ -44,7 +40,6 @@ sudo systemctl restart docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-
 ## Download Kubernetes 1.19
 mkdir -p 03_kubernetes
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
@@ -52,13 +47,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
 sudo apt-get update
 sudo apt-get install --reinstall -y kubelet=1.19.6-00 kubeadm=1.19.6-00 kubectl=1.19.6-00 --download-only
 sudo mv /var/cache/apt/archives/*.deb 03_kubernetes/
 sudo apt-get install --reinstall -y kubelet=1.19.6-00 kubeadm=1.19.6-00 kubectl=1.19.6-00
 sudo rm /var/cache/apt/archives/*.deb
-
 ## Download nvidia-docker-2
 mkdir -p 04_nvidiadocker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
@@ -68,7 +61,6 @@ sudo apt-get update && sudo apt-get install --reinstall -y nvidia-docker2 --down
 sudo mv /var/cache/apt/archives/*.deb 04_nvidiadocker/
 sudo apt-get update && sudo apt-get install --reinstall -y nvidia-docker2
 sudo rm /var/cache/apt/archives/*.deb
-
 ## Download docker images
 mkdir -p 05_dockerimages
 kubeadm config images pull
@@ -77,13 +69,9 @@ docker pull quay.io/coreos/flannel:v0.13.1-rc1
 docker pull nvidia/k8s-device-plugin:v0.7.3
 echo Saving images to file
 sudo docker save $(sudo docker images | sed '1d' | awk '{print $1 ":" $2 }') -o 05_dockerimages/dockerimages.tar
-
 ## Download/Create config files
 mkdir -p 06_config
 wget https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.7.3/nvidia-device-plugin.yml
 wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 mv nvidia-device-plugin.yml 06_config
 mv kube-flannel.yml 06_config
-
-
-
